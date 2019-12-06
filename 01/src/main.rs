c0a1f7302515d73baea1833ce2fa8d2903eaf78c
Jamie Kelly <git@jamiekelly.com>
2019-12-06 23:50:08 +0000

    Refactor day 1

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,38 +1,31 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
+use std::fs::read_to_string;
+use std::iter::successors;
 
 fn main() {
-    let input_file_name: &str = "input.txt";
+    let masses: Vec<isize> = read_to_string("input.txt")
+        .unwrap()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
 
-    let mut masses: Vec<i32> = Vec::new();
-    for input in read_file(input_file_name).iter() {
-        masses.push(input.parse().unwrap())
-    }
+    let fuel_for_mass: isize = masses.clone().into_iter().map(calculate_fuel).sum();
+    println!("Fuel for mass: {}", fuel_for_mass);
 
-    let mut fuel_total: i32 = 0;
-
-    for mass in masses.iter() {
-        let mut fuel_required = fuel_for_mass(&mass);
-        fuel_total = fuel_total + fuel_required;
-        while fuel_for_mass(&fuel_required) > 0 {
-            fuel_required = fuel_for_mass(&fuel_required);
-            fuel_total = fuel_total + fuel_required;
-        }
-    }
-
-    println!("Total fuel required: {}", fuel_total);
+    let total_fuel: isize = masses
+        .into_iter()
+        .map(calculate_fuel)
+        .flat_map(calculate_fuel_for_fuel)
+        .sum();
+    println!("Total fuel required: {}", total_fuel);
 }
 
-fn read_file(filename: &str) -> Vec<String> {
-    let file = File::open(filename).expect("No such file");
-
-    let buffer = BufReader::new(file);
-
-    let coll: Vec<String> = buffer.lines().collect::<Result<_,_>>().unwrap();
-    coll
+fn calculate_fuel(mass: isize) -> isize {
+    0.max((mass / 3) - 2)
 }
 
-fn fuel_for_mass(mass: &i32) -> i32 {
-    (mass / 3) - 2
+fn calculate_fuel_for_fuel(fuel: isize) -> impl Iterator<Item = isize> {
+    successors(Some(fuel), |&fuel| match calculate_fuel(fuel) {
+        0 => None,
+        _ => Some(calculate_fuel(fuel)),
+    })
 }
